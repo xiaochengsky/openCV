5790091cc7053775e9581e77124a04a7ba96a67b
xiaochengsky <626576619@qq.com>
2020-02-18 17:24:53 +0800

    图像的形态学——形态学梯度

--- tutorial_014/tutorial_014.py
+++ tutorial_014/tutorial_014.py
@@ -11,7 +11,7 @@ print('img.shape: ', img.shape)
 # 设置旋转参数: 旋转中心, 旋转角度, 旋转后的缩放因子
 M = cv2.getRotationMatrix2D((w/2, h/2), 45, 0.6)
 
-# 获取平移图像
+# 获取旋转图像
 dst = cv2.warpAffine(img, M, (2*w, 2*h))
 cv2.imshow('dst', dst)
 print('dst.shape: ', dst.shape)
