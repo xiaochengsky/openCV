e0b71afa469e924cdb3f5b766d838ae82d116ed0
xiaochengsky <626576619@qq.com>
2020-02-20 16:07:45 +0800

    图像的二值分析——轮廓近似

--- tutorial_043/tutorial_043.py
+++ tutorial_043/tutorial_043.py
@@ -15,14 +15,14 @@ def threshold_demo(image):
     return binary
 
 
-img = cv2.imread('test.png')
+img = cv2.imread('pic3.png')
 
 # 避免修改原图
 img = img.copy()
 binary = threshold_demo(img)
 
 # 轮廓发现
-out, contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+out, contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 for i in range(len(contours)):
     # 计算包围目标的最小矩形区域
     rect = cv2.minAreaRect(contours[i])
@@ -32,6 +32,7 @@ for i in range(len(contours)):
     epsilon = 0.05 * cv2.arcLength(contours[i], True)
     result = cv2.approxPolyDP(contours[i], epsilon, True)
     vertexes = result.shape[0]
+    cv2.drawContours(img, contours, -1, (0, 0, 255), 3)
     cv2.putText(img, str(vertexes), (np.int32(cx), np.int32(cy)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255))
 
 
